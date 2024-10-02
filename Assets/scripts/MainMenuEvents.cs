e291029aa54be7725ad005dd4e94e54c8a1aa2bf
minecrafterMc <78699603+minecrafterMc@users.noreply.github.com>
2024-10-02 20:27:34 +0200

    initial commit
    initial commit

---
+++ Assets/scripts/MainMenuEvents.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MainMenuEvents : MonoBehaviour
+{
+    private UIDocument _document;
+
+    private Button _button;
+
+    private Toggle _checkbox;
+
+    private List<Button> _menuButtons = new List<Button>();
+
+    private void Awake(){
+        _document = GetComponent<UIDocument>();
+
+        _button = _document.rootVisualElement.Q("openModeEditor") as Button;
+        _checkbox = _document.rootVisualElement.Q("testToggle") as Toggle;
+        _button.RegisterCallback<ClickEvent>(OnPlayerGameClick);
+
+        _menuButtons = _document.rootVisualElement.Query<Button>().ToList();
+        for (int i = 0; i < _menuButtons.Count;i++){
+            _menuButtons[i].RegisterCallback<ClickEvent>(OnAnyButtonClick);
+        }
+    }
+
+    private void OnDisable(){
+        _button.UnregisterCallback<ClickEvent>(OnPlayerGameClick);
+        for (int i = 0; i < _menuButtons.Count;i++){
+            _menuButtons[i].UnregisterCallback<ClickEvent>(OnAnyButtonClick);
+        }
+    }
+
+    private void OnPlayerGameClick(ClickEvent evt){
+        Debug.Log(_checkbox.value);
+    }
+    public void OnAnyButtonClick(ClickEvent evt){
+        Debug.Log("all test");
+    }
+}
