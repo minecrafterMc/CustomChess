d13de358e42f2c93f3de71984fa768feac804641
minecrafterMc <78699603+minecrafterMc@users.noreply.github.com>
2024-10-07 21:40:20 +0200

    some more data changes

--- Assets/scripts/modeData/shape.cs
+++ Assets/scripts/modeData/shape.cs
@@ -9,8 +9,7 @@ public class shape : ScriptableObject
     [SerializeField] private modeData _parentMode;
     public modeData parentMode {get => _parentMode;}
     [SerializeField] public string _name;
-    [SerializeField] public int moveDistanceLimit;
-    [SerializeField] public bool canMoveInfinitely;
+    [SerializeField] public bool isImportant;
     //array values:
     //0- cant move or attack
     //1- can move but not attack
@@ -29,8 +28,12 @@ public class shape : ScriptableObject
     //9- horse down right
     //10- horse down left
     //11- horse up left
+    [Header("movement")]
     [SerializeField] public int[] moveDirections = {0,0,0,0,0,0,0,0,0,0,0,0};
-    [SerializeField] public bool isImportant;
+    [SerializeField] public int[] moveDistances = {0,0,0,0,0,0,0,0,0,0,0,0};
+    [SerializeField] public bool UsesFirstMoveBoost;
+    [SerializeField] public int[] moveFirstDistances = {0,0,0,0,0,0,0,0,0,0,0,0};
+    
 
 #if UNITY_EDITOR
     public void Initialise (modeData parentMode){
@@ -38,14 +41,14 @@ public class shape : ScriptableObject
     }
 #endif
 #if UNITY_EDITOR
-[ContextMenu("Rename to name")]
+[ContextMenu("Update Editor Name")]
     private void Rename(){
         this.name = _name;
         EditorUtility.SetDirty(this);
     }
 #endif
 #if UNITY_EDITOR
-[ContextMenu("Delete this")]
+[ContextMenu("Delete this shape")]
     private void DeleteThis(){
         _parentMode.shapes.Remove(this);
         Undo.DestroyObjectImmediate(this);
